697a5b0af7ea5681e70c354a50cebd89ed9f3a80
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2022-04-05 20:32:36 +0100

    migrations

---
+++ database/migrations/2022_03_20_094155_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->string('thumbnail')->nullable();
+            $table->text('content');
+            $table->text('excerpt');
+            $table->timestamp('published_at')->nullable();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')
+            ->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
