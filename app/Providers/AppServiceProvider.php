fc0a494d0cd721ee1f41842bf990865642a301a2
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2022-04-05 20:36:14 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Services\MailchimpNewsletter;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
+use App\Services\Newsletter;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        app()->bind(Newsletter::class, function(){
+            $client = (new ApiClient())->setConfig(collect(config('services.mailchimp'))->only('apiKey', 'server'));
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
@@ -23,6 +32,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('admin', fn(User $user) => $user->role === 'administrator');
+
+        Blade::if('admin', fn() => request()->user() && request()->user()->can('admin'));
     }
 }
