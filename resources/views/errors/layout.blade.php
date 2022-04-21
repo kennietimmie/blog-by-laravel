2c42ac6f75a5795ff2f6d913da186b400f3b8331
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2022-04-21 14:16:50 +0100

    ops

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -49,7 +49,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title">
-                    @yield('message')
+                   Ops, @yield('message')
                 </div>
             </div>
         </div>
