356eacd17f6b0c11d463856f8b624cd3a3f5a29f
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2022-04-24 18:42:10 +0100

    listening to the event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Events\PostDelete;
+use App\Listeners\PostEventSubscriber;
+use App\Listeners\SendPostDeletedNotification;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        PostDelete::class => [
+            SendPostDeletedNotification::class
+        ]
+    ];
+
     ];
 
     /**
