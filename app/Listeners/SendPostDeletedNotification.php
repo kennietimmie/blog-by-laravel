5cefcafe8dcf44bd5de05201ababcc12bb5b6141
Kehinde Sholadoye <kehinde.bridgewateracademy@gmail.com>
2022-04-26 19:31:12 +0100

    Update SendPostDeletedNotification.php

--- app/Listeners/SendPostDeletedNotification.php
+++ app/Listeners/SendPostDeletedNotification.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\PostDelete;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 
 class SendPostDeletedNotification implements ShouldQueue
@@ -27,7 +28,7 @@ class SendPostDeletedNotification implements ShouldQueue
      */
     public function handle(PostDelete $event)
     {
-        Log::channel('post-delete')->info('Post deleted by user: ' . auth()->user()->username, collect($event->post)->except(['author', 'categories'])->toArray());
+        Log::channel('post-delete-info')->info('Post deleted by user: ' . auth()->user()->username, collect($event->post)->except(['author', 'categories'])->toArray());
     }
 
     /**
@@ -39,6 +40,11 @@ class SendPostDeletedNotification implements ShouldQueue
      */
     public function failed(PostDelete $event, $exception)
     {
-        //
+        $string = 'The error took place between :start and :end';
+        $replaced = preg_replace_array('/:[a-z_]+/', [now()->toTimeString(), now()->toTimeString()], $string);
+
+        Log::channel('post-delete-error')->error('An error ocurred while user: '
+        . auth()->user()->username . 'was deleting post: '. $event->post->title . '('. $event->post->id . ')'
+        . $replaced, Arr::except($event->post->toArray(), ['author', 'categories']), collect($exception));
     }
 }
